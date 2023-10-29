f713b2f32f7a7c611071551e979ffb6c269d6bb7
afsun7 <omidi.Afsaneh7@gmail.com>
2023-10-30 02:01:44 +0330

    edit

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -13,32 +13,33 @@ export default function Profile() {
   setName(name);
 
   return (
-    <div className="-mt-4">
-      <div className="mb-2">
-        <Link to="/">
-          <button className="px-5 py-2  bg-[#f6f6f6] text-[#898989]">
-            Back To Search
-          </button>
-        </Link>
-        <span className="ml-2">
-          hireable :
-          {user?.hireable ? (
-            <i className="fa fa-check text-green-700 "></i>
-          ) : (
-            <i className="fa fa-close text-red-600"></i>
-          )}
-        </span>
-      </div>
-      <Bio />
-      <Follow />
-      <Repository />
+    <>
       {loadingUser ? (
         <div className="m-auto w-[4%] mt-20 ">
           <img src={loading}></img>
         </div>
       ) : (
-        <></>
+        <div className="-mt-4">
+          <div className="mb-2">
+            <Link to="/">
+              <button className="px-5 py-2  bg-[#f6f6f6] text-[#898989]">
+                Back To Search
+              </button>
+            </Link>
+            <span className="ml-2">
+              hireable :
+              {user?.hireable ? (
+                <i className="fa fa-check text-green-700 "></i>
+              ) : (
+                <i className="fa fa-close text-red-600"></i>
+              )}
+            </span>
+          </div>
+          <Bio />
+          <Follow />
+          <Repository />
+        </div>
       )}
-    </div>
+    </>
   );
 }
